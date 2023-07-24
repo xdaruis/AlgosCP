7c99e339fb5c5c8b8ea0ae9123dc405288ce7c80
Rozemberg Darius Andrei <135977849+xdaruis@users.noreply.github.com>
2023-07-24 22:34:00 +0200

    Add files via upload

---
+++ packages/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('website.urls')),
+    path('users/', include('django.contrib.auth.urls')),
+    path('users/', include('users.urls')),
+]
---
+++ users/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+# Create your views here.
+def userLogin(request):
+    return render(request, 'auth/login.html', {})
