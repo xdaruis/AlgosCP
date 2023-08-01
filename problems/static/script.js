2aee63d8dbe979966bc044d46607219c104f7b51
xdaruis <rozdaruis@gmail.com>
2023-08-01 20:02:18 +0300

    Implemented problems + code editor

--- problems/static/script.js
+++ problems/static/script.js
@@ -1,6 +1,9 @@
-alert("HI");
-let editor = document.querySelector("#editor");
+let editor = document.getElementById('editor');
 
 ace.edit(editor, {
-    theme: 'ace/theme/cobalt',
-})
+    theme: 'ace/theme/chrome',
+    mode: 'ace/mode/c_cpp',
+    tabSize: 4,
+    useSoftTabs: true,
+    keyboardHandler: 'ace/keyboard/vscode',
+});
--- problems/views.py
+++ problems/views.py
@@ -4,7 +4,7 @@ from .models import Problem
 # Create your views here.
 def list(request):
     problems = Problem.objects.all()
-    return render(request, 'problem_list.html', {'problems': problems})
+    return render(request, 'problems_list.html', {'problems': problems})
 
 def problem(request, problem_name):
     problem = Problem.objects.get(name = problem_name.replace('-', ' '))
