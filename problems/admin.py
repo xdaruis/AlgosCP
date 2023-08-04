17490fb80299e42d6c310717ee084ec560af7d07
xdaruis <rozdaruis@gmail.com>
2023-08-04 22:38:52 +0300

    Added Better Markdown Editor

--- problems/admin.py
+++ problems/admin.py
@@ -3,14 +3,9 @@ from .models import Problem, Submission
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-# admin.site.register(Problem)
-
 admin.site.register(Submission)
 
-
-# Apply summernote to all TextField in model.
-class SomeModelAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
-    # summernote_fields = '__all__'
+class SomeModelAdmin(SummernoteModelAdmin):
     summernote_fields = ('description', 'constraints')
 
 admin.site.register(Problem, SomeModelAdmin)
--- users/admin.py
+++ users/admin.py
@@ -3,6 +3,4 @@ from django.contrib.auth.models import Group
 from .models import Profile
 
 # Register your models here.
-# admin.site.unregister(Group)
-
 admin.site.register(Profile)
