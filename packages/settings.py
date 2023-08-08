22a38849d7516b89254489b769f68eba6c48cd5c
xdaruis <rozdaruis@gmail.com>
2023-08-08 15:55:01 +0300

    SQLite for dev environment

--- packages/settings.py
+++ packages/settings.py
@@ -82,23 +82,23 @@ WSGI_APPLICATION = 'packages.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'website-database',
-        'USER': 'postgres',
-        'PASSWORD': os.getenv("PASSWORD"),
-        'PORT': '5432',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'website-database',
+#         'USER': 'postgres',
+#         'PASSWORD': os.getenv("PASSWORD"),
+#         'PORT': '5432',
+#         'HOST': 'localhost'
+#     }
+# }
 
 
 # Password validation
