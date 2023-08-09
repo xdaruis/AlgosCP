125db3378e2b251c6bacd6f8deec561674cd8699
daruis <rozdaruis@gmail.com>
2023-08-09 16:29:16 +0300

    script

--- Evaluator/run.py
+++ Evaluator/run.py
@@ -11,6 +11,8 @@ def test_solution():
     try:
         compile_program = f"g++ {base_path}/solution.cpp -o {base_path}/solution"
         subprocess.run(compile_program, shell=True, check=True)
+        # memory = 1024 * 256
+        # memory_limit = f"ulimit -v {memory}"
         execute_program = f"timeout 5 {base_path}/solution > {base_path}/solution.out"
         subprocess.run(execute_program, shell=True, check=True)
         compare_results = f"diff {base_path}/correct_solution.txt {base_path}/solution.out > {base_path}/errors.txt"
