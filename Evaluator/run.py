5c485f81446f2f795c319088d7bba8d354dd380a
daruis <rozdaruis@gmail.com>
2023-08-09 16:12:51 +0300

    script

--- Evaluator/run.py
+++ Evaluator/run.py
@@ -1,23 +1,30 @@
 import subprocess
 
 def test_solution():
+    base_path = "/home/daruis/PersProject/AlgosCP/Evaluator"
     try:
         move_file_from_downloads = "mv /home/daruis/Downloads/solution.cpp /home/daruis/PersProject/AlgosCP/Evaluator"
         subprocess.run(move_file_from_downloads, shell=True, check=True)
-        base_path = "/home/daruis/PersProject/AlgosCP/Evaluator"
+    except:
+        print("Files Missing! Internal server error")
+        return
+    try:
         compile_program = f"g++ {base_path}/solution.cpp -o {base_path}/solution"
         subprocess.run(compile_program, shell=True, check=True)
-        execute_program = f"{base_path}/solution > {base_path}/solution.out"
+        execute_program = f"timeout 5 {base_path}/solution > {base_path}/solution.out"
         subprocess.run(execute_program, shell=True, check=True)
         compare_results = f"diff {base_path}/correct_solution.txt {base_path}/solution.out > {base_path}/errors.txt"
         try:
             subprocess.run(compare_results, shell=True, check=True)
         except:
             print("WRONG ANSWER")
-        cleanup_files = f"rm {base_path}/solution {base_path}/solution.cpp {base_path}/solution.out"
-        subprocess.run(cleanup_files, shell=True, check=True)
-    except:
-        print("Failed Compilation")
+    except subprocess.CalledProcessError as e:
+        if e.returncode == 124:
+            print("Time limit exceeded!")
+        else:
+            print("Failed Compilation")
+    cleanup_files = f"rm {base_path}/solution {base_path}/solution.cpp {base_path}/solution.out"
+    subprocess.run(cleanup_files, shell=True, check=True)
 
 if __name__ == '__main__': 
     test_solution()
