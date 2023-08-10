85efbb71fc75fa9104dcb550f007b1a4a8e1b32b
daruis <rozdaruis@gmail.com>
2023-08-10 17:57:40 +0300

    testcases and time limit

--- Evaluator/run.py
+++ Evaluator/run.py
@@ -1,10 +1,12 @@
 import subprocess
+import re
 
 def test_solution():
     base_path = "/home/daruis/PersProject/AlgosCP/Evaluator"
     try:
         move_file_from_downloads = "mv /home/daruis/Downloads/solution.cpp /home/daruis/PersProject/AlgosCP/Evaluator"
         subprocess.run(move_file_from_downloads, shell=True, check=True)
+        add_input()
     except:
         print("Files Missing! Internal server error")
         return
@@ -28,5 +30,16 @@ def test_solution():
     cleanup_files = f"rm {base_path}/solution {base_path}/solution.cpp {base_path}/solution.out"
     subprocess.run(cleanup_files, shell=True, check=True)
 
-if __name__ == '__main__': 
+def add_input():
+    cpp_filename = "solution.cpp"
+    start_testing = 'freopen("test_cases.in", "r", stdin); int _test; cin >> _test; while(_test--) {'
+    end_testing = ' cout << " "; }'
+    with open(cpp_filename, 'r') as cpp_file:
+        cpp_content = cpp_file.read()
+    modified_content = re.sub(r'(int\s+main\s*\(\s*\)\s*{)', rf'\1\n    {start_testing}\n', cpp_content, count=1)
+    modified_content = re.sub(r'(\s*return\s+0\s*;)', rf'    {end_testing}\n\1', modified_content)
+    with open(cpp_filename, 'w') as cpp_file:
+        cpp_file.write(modified_content)
+
+if __name__ == '__main__':
     test_solution()
