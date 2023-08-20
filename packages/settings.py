5fbf41c38260c9547b234061a75e8c90ade1da6d
xdaruis <rozdaruis@gmail.com>
2023-08-20 22:36:43 +0300

    db

--- packages/settings.py
+++ packages/settings.py
@@ -84,21 +84,14 @@ WSGI_APPLICATION = 'packages.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'website-database',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv("PASSWORD"),
+        'PORT': '5432',
+        'HOST': 'localhost'
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'website-database',
-#         'USER': 'postgres',
-#         'PASSWORD': os.getenv("PASSWORD"),
-#         'PORT': '5432',
-#         'HOST': 'localhost'
-#     }
-# }
 
 
 # Password validation
