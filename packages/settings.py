0f1af91f363a56fe1951b166e66901d2f2f2025c
xdaruis <rozdaruis@gmail.com>
2023-09-05 17:43:58 +0300

    Fixed Settings

--- packages/settings.py
+++ packages/settings.py
@@ -135,7 +135,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # API URL
-EVALUATOR_API_URL = 'http://localhost:8000/api/test-submission/'
+EVALUATOR_API_URL = os.getenv("API_LINK")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
